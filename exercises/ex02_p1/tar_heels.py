4920fc7d63b0b9180ce80d560383ba22e813de07
Madelein Ngo <wumeijin@live.unc.edu>
2021-02-10 16:56:17 -0500

    after autograder attempt 1

--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -10,17 +10,18 @@ def main() -> None:
 
 
 # TODO 1: Define the tar_heels function, and its logic, here.
-def tar_heels(choice:int) -> str:
-    if choice % 2 == 0 and choice % 7 == 0:
+def tar_heels(choice:int) -> str: 
+    if choice % 2 == 0 and choice % 7 == 0: 
         return "TAR HEELS"
     else:
-        if choice % 2 == 0:
+        if choice % 2 == 0: 
             return "TAR"    
         else:
-            if choice % 7 == 0:
+            if choice % 7 == 0: 
                 return "HEELS"
             else:
                 return "CAROLINA"
+
 
 if __name__ == "__main__":
     main()
