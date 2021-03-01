c721c74e502dcdad4f955645c9832b15b8882fe4
Madelein Ngo <wumeijin@live.unc.edu>
2021-02-28 19:50:09 -0500

    finished first draft before autograder

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -12,30 +12,31 @@ def main() -> None:
     doses_per_day: int = int(input("Doses per day: "))
     target: int = int(input("Target percent vaccinated: "))
     # TODO 2: Call days_to_target and store result in a variable.
-    days_new: int = days_to_target(population, doses, doses_per_day, target)
-    print(days_new)
+    days: int = days_to_target(population, doses, doses_per_day, target)
     # TODO 4: Call future_date and store result in a variable.
-    day_vac_datetime: str = future_date(days_new)
+    day_datetime: str = future_date(days)
     # TODO 5: Print the expected output using the variables above.
     target_str: str = str(target)
-    days_new_str: str = str(days_new)
-    print("We will reach " + target_str + "% vaccination in " + days_new_str + " days, which falls on " + day_vac_datetime + ".")
+    days_str: str = str(days)
+    print("We will reach " + target_str + "% vaccination in " + days_str + " days, which falls on " + day_datetime + ".")
 
 
 # TODO 1: Define days_to_target function
 def days_to_target(population: int, doses: int, doses_per_day: int, target: int) -> int:
-    target_percent: int = int(target * 0.01)
-    target_population: int = int(target_percent * population)
-    amount_doses: int = int(target_population * 2)
-    days: int = int(amount_doses // doses_per_day)
-    days_started: int = int(doses // doses_per_day)
-    days_new: int = int(days - days_started)
-    days_new: int = int(round(days_new))
-    return days_new
+    """Calculates days to reach target vaccination."""
+    target_percent: float = float(target * 0.01)
+    current_percent: float = float(doses // (population * 2))
+    x: float = float(target_percent - current_percent)
+    round(x)
+    vaccines_still_needed: int = int((x * population) * 2)
+    days: int = int(vaccines_still_needed // doses_per_day)
+    round(days)
+    return days
 
 
 # TODO 3: Define future_date function
-def future_date (days_new: int) -> str:
+def future_date(days_new: int) -> str:
+    """Calculates date of when target vaccination reached."""
     today: datetime = datetime.today()
     day_vac: timedelta = timedelta(days_new)
     day_vac_datetime: datetime = today + day_vac
---
+++ projects/cyoa.py
@@ -0,0 +1,133 @@
+"""First project for COMP 110."""
+
+__author__ = "730272774"
+
+def main() -> None:
+    """Entrypoint of the program."""
+    greet()
+    PLAYER: str = str("")
+    POINTS: int = int(0)
+    EMOJI: str = str("\U0001FD90")
+    choice: str = str(input("What would you like to do? explore room 1/explore room 2/exit"))
+    if choice == "exit":
+        return "Thanks for playing, see you later!"
+    else:
+        if choice == "explore room 1":
+            room_1()
+        else:
+            if choice == "explore room 2":
+                room_2(points)
+
+
+def greet() -> None:
+    """Greets the player when beginning game."""
+    player: str(input("Hi! Welcome, what's your name?"))
+    print(f"Hi {player}! Glad you could join us for this choose your own adventure game.")
+    return
+
+
+def room_1() -> None:
+    """Starts exploration of room 1."""
+    print(f"This game experience will have you search through mystery room 1 {emoji}, try to guess what the purpose of the room is!")
+    print("Let's get to it!")
+    choice_1: int(input(f"To begin,{player}, how many letters are in your name?"))
+    choice_1_added: int = randint(1, 100) + choice_1
+    if choice_1_added % 2 == 0:
+        print("\U0001F34E")
+        global points
+        points += 100
+        print("Congratualations, you've found an apple! You get one hundred points!")
+    else: 
+        print("\U0001F34A")
+        global points
+        points += 50
+        print("Congratualtions, you've found an orange! You get 100 points!")
+    choice_2: str(input("Nice! You've found one fruit. Now choose between option one or two."))
+    if choice_2 == "one":
+        print("\U0001F9FA")
+        global points
+        points += 100
+        print("You've found a basket! You get 100 more points.")
+    else:
+        print("\U0001F37D")
+        global points 
+        points += 200
+        print("Wow, you've found a plate and utensils kit! You get 200 more points.")
+    choice_3: str(input("Last choice, hot or cold?"))
+    if choice_3 == "hot":
+        print("\U00012615")
+        global points
+        points += 200
+        print("Nice, you've got a hot beverage! You get 200 more points.")
+    else:
+        print("\U0001F379")
+        global points
+        points += 150
+        print("Nice, you've got a tropical drink! You get 150 more points.")
+    final_choice: str(input("Can you guess what this room is for?"))
+    if final_choice == "picnic":
+        print("\U0001F60E")
+        global points
+        points += 1000
+        print("Amazing! You guessed it correctly! Therefore you get 1000 points!")
+    else:
+        if final_choice == "food":
+            print("\U0001F609")
+            global points 
+            points += 500
+            print("You're really close! But this room is specifically for a picnic. You get 500 points!")
+        else:
+            print("\U0001F62E")
+            global points
+            points += 100
+            print("Sorry, that's not exactly it. This room is for a picnic! But you still get 100 points for guessing.")
+    print("Thanks for exploring this room! You can continue to explore the other room or quit at main!")
+    return 
+    
+
+
+def room_2(points: int) -> int:
+    """Starts exploration of room 2."""
+    print(f"This game experience will have you search through mystery room 2 {emoji}, try to guess what the purpose of the room is!")
+    print("Let's get to it!")
+    choice_1: str(input(f"First, {player}, how many vowels does your name have?"))
+    if choice_1 == "2":
+        print("\U0001F33B")
+        points += 100
+        print("You found a sunflower! 100 points are awarded to you.")
+    else:
+        print("\U0001F335")
+        points += 75
+        print("You found a catcus! 75 points are awarded to you.")
+    choice_2: str(input("Next, water or air?"))
+    if choice_2 == "water":
+        print("\U0001F438")
+        points += 150
+        print("You've found a nice frog boy! Here's 100 points for you!")
+    else:
+        print("\U0001F98B")
+        points += 100
+        print("You've found a nice butterfly! Here's 100 points for you!")
+    choice_3: str(input("Last choice, round or straight?")
+    if choice_3 == "round":
+        print("\U0001F344")
+        points += 150
+        print("You've found a big mushroom to sit on. 150 points for you!")
+    else:
+        print("\U0001FAB5")
+        points += 100
+        print("You've found a sturdy wooden log to sit on. 100 points for you!")
+    final_choice: str(input("Now that you've got all your things, what do you think this room is?"))
+    if final_choice == "garden":
+        print("\U0001F3E1")
+        points += 1000
+        print("Wow, you got it! This is the garden of the house. For that amazingly accurate guess, you get 1000 points!")
+    else:
+        print("\U0001F140")
+        points += 100
+        print("Not quite, this room is the garden of the house. For your guess though, you get 100 points!")
+    return points
+
+
+if __name__ == "__main__":
+    main()
