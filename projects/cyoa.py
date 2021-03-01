26d88d0b00bf73ce052f19eb2eb9412d3330c033
Madelein Ngo <wumeijin@live.unc.edu>
2021-02-28 20:24:05 -0500

    after round 2 of autograder feedback

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -11,19 +11,20 @@ def main() -> None:
     EMOJI: str = str("\U0001FD90")
     choice: str = str(input("What would you like to do? explore room 1/explore room 2/exit"))
     if choice == "exit":
-        return "Thanks for playing, see you later!"
+        print("Thanks for playing, see you later!")
+        return
     else:
         if choice == "explore room 1":
             room_1()
         else:
             if choice == "explore room 2":
-                room_2(points)
+                room_2(0)
 
 
 def greet() -> None:
     """Greets the player when beginning game."""
     global PLAYER
-    PLAYER: str(input("Hi! Welcome, what's your name?"))
+    PLAYER: str[input("Hi! Welcome, what's your name?")]
     print(f"Hi {PLAYER}! Glad you could join us for this choose your own adventure game.")
     return
 
@@ -34,7 +35,7 @@ def room_1() -> None:
     global PLAYER
     print(f"This game experience will have you search through mystery room 1 {EMOJI}!")
     print("Let's get to it!")
-    choice_1: int(input(f"To begin,{PLAYER}, how many letters are in your name?"))
+    choice_1: int[input(f"To begin,{PLAYER}, how many letters are in your name?")]
     choice_1_added: int = randint(1, 100) + choice_1
     global POINTS
     if choice_1_added % 2 == 0:
@@ -45,7 +46,7 @@ def room_1() -> None:
         print("\U0001F34A")
         POINTS += 50
         print("Congratualtions, you've found an orange! You get 100 points!")
-    choice_2: str(input("Nice! You've found one fruit. Now choose between option one or two."))
+    choice_2: str[input("Nice! You've found one fruit. Now choose between option one or two.")]
     if choice_2 == "one":
         print("\U0001F9FA")
         POINTS += 100
@@ -54,7 +55,7 @@ def room_1() -> None:
         print("\U0001F37D")
         POINTS += 200
         print("Wow, you've found a plate and utensils kit! You get 200 more points.")
-    choice_3: str(input("Last choice, hot or cold?"))
+    choice_3: str[input("Last choice, hot or cold?")]
     if choice_3 == "hot":
         print("\U00012615")
         POINTS += 200
@@ -63,7 +64,7 @@ def room_1() -> None:
         print("\U0001F379")
         POINTS += 150
         print("Nice, you've got a tropical drink! You get 150 more points.")
-    final_choice: str(input("Can you guess what this room is for?"))
+    final_choice: str[input("Can you guess what this room is for?")]
     if final_choice == "picnic":
         print("\U0001F60E")
         POINTS += 1000
@@ -87,7 +88,7 @@ def room_2(points: int) -> int:
     global PLAYER
     print(f"This game experience will have you search through mystery room 2 {EMOJI}.")
     print("Let's get to it!")
-    choice_1: str(input(f"First, {PLAYER}, how many vowels does your name have?"))
+    choice_1: str[input(f"First, {PLAYER}, how many vowels does your name have?")]
     if choice_1 == "2":
         print("\U0001F33B")
         points += 100
@@ -96,7 +97,7 @@ def room_2(points: int) -> int:
         print("\U0001F335")
         points += 75
         print("You found a catcus! 75 points are awarded to you.")
-    choice_2: str(input("Next, water or air?"))
+    choice_2: str[input("Next, water or air?")]
     if choice_2 == "water":
         print("\U0001F438")
         points += 150
@@ -105,7 +106,7 @@ def room_2(points: int) -> int:
         print("\U0001F98B")
         points += 100
         print("You've found a nice butterfly! Here's 100 points for you!")
-    choice_3: str(input("Last choice, round or straight?"))
+    choice_3: str[input("Last choice, round or straight?")]
     if choice_3 == "round":
         print("\U0001F344")
         points += 150
@@ -114,7 +115,7 @@ def room_2(points: int) -> int:
         print("\U0001FAB5")
         points += 100
         print("You've found a sturdy wooden log to sit on. 100 points for you!")
-    final_choice: str(input("Now that you've got all your things, what do you think this room is?"))
+    final_choice: str[input("Now that you've got all your things, what do you think this room is?")]
     if final_choice == "garden":
         print("\U0001F3E1")
         points += 1000
