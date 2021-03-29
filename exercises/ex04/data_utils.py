a229f08ec5c6e931c2249c7667806df803c4d8d5
Madelein Ngo <wumeijin@live.unc.edu>
2021-03-28 23:51:20 -0400

    finished exercise!

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -20,6 +20,7 @@ def read_csv_rows(csv_file: str) -> list[dict[str, str]]:
 
 # TODO: Define the other functions here.
 def column_values(rows: list[dict[str, str]], column: str) -> list[str]:
+    """Producing a list of column values."""
     columns: list[str] = []
     for row in rows:
         columns.append(row[column])
@@ -27,7 +28,40 @@ def column_values(rows: list[dict[str, str]], column: str) -> list[str]:
 
 
 def columnar(rows: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Making a column based table."""
     columns: dict[str, list[str]] = {}
     for column in rows[0]:
         columns[column] = column_values(rows, column)
     return columns
+
+
+def head(columns: dict[str, list[str]], n: int) -> dict[str, list[str]]:
+    """Making sure column based table is working."""
+    head_columns: dict[str, list[str]] = {}
+    if n > len(columns):
+        n = len(columns)
+    for column in columns:
+        n_values: list[str] = []
+        for row in range(n):
+            n_values.append(columns[column][row])
+        head_columns[column] = n_values
+    return head_columns
+
+
+def select(columns: dict[str, list[str]], names: list[str]) -> dict[str, list[str]]:
+    """Selects a subset of column names."""
+    select_columns: dict[str, list[str]] = {}
+    for column in names:
+        select_columns[column] = columns[column]
+    return select_columns
+
+
+def count(selected_data: list[str]) -> dict[str, int]:
+    """Counts frequency of column values."""
+    counts: dict[str, int] = {}
+    for item in selected_data:
+        if item in counts:
+            counts[item] += 1
+        else:
+            counts[item]: = 1
+    return counts
