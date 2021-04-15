edada2c57418604d6a6495f5713200613f0a79c5
Madelein Ngo <wumeijin@live.unc.edu>
2021-04-15 16:12:19 -0400

    starting ex05 and ls23

--- exercises/ex05/Simpy.py
+++ exercises/ex05/Simpy.py
@@ -4,10 +4,12 @@ from __future__ import annotations
 
 from typing import Union
 
-__author__ = "YOUR PID HERE"
+__author__ = "730272774"
 
 
 class Simpy:
     values: list[float]
 
     # TODO: Your constructor and methods will go here.
+    def __init__(self, values: list[float]) -> list[float]:
+        self = value
---
+++ lessons/ls23_strarray.py
@@ -0,0 +1,40 @@
+"""Examples of vectorized operation on StrArray."""
+
+from __future__ import annotations
+
+from typing import Union
+
+
+class StrArray:
+    """Utility class for common string column operations."""
+
+    values: list[str]
+
+    def __init__(self, values: list[str]):
+        self.values = values
+
+    def __repr__(self) -> str:
+        return f"StrArray({self.values})"
+
+    def __add__(self, rhs: Union[str, StrArray]) -> StrArray:
+        result: list[str] = []
+        if isinstance(rhs, str):
+            result: list[str] = []
+            for item in self.values:
+                result.append(item + rhs) 
+        else:
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                result.append(self.values[i] + rhs.values[i])
+        return StrArray(result)
+
+
+s: StrArray = StrArray(["a", "b", "c"])
+t = StrArray(["d", "e", "f"])
+print(s + " wow!")
+print(s + t)
+
+first = StrArray(["Kris", "Kaki", "Maddie"])
+last = StrArray(["Jordan", "Ryan", "B"])
+full_name = last + ", " + first
+print(full_name)
