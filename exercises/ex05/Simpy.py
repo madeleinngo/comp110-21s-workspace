40a1f24f6d75e5f8b06e0f02a05c890af895c393
Madelein Ngo <wumeijin@live.unc.edu>
2021-04-19 12:52:37 -0400

    starting ex05

--- exercises/ex05/Simpy.py
+++ exercises/ex05/Simpy.py
@@ -11,5 +11,15 @@ class Simpy:
     values: list[float]
 
     # TODO: Your constructor and methods will go here.
-    def __init__(self, values: list[float]) -> list[float]:
-        self = value
+    def __init__(self, values: list[float]):
+        self.values = values
+
+    def __repr__(self) -> str:
+        return f"Simpy({self.values})"
+
+    def fill(self, fill: float, number: int) -> None:
+        if len(self.values) < number:
+            for item in self.values:
+                self.values = fill
+
+        return self.values 
