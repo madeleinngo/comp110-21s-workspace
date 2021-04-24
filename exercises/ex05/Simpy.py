c572cd264eef85f5c241ec1b91528f6f8b3a7b76
Madelein Ngo <wumeijin@live.unc.edu>
2021-04-24 14:55:05 -0400

    progress on ex05

--- exercises/ex05/Simpy.py
+++ exercises/ex05/Simpy.py
@@ -18,8 +18,88 @@ class Simpy:
         return f"Simpy({self.values})"
 
     def fill(self, fill: float, number: int) -> None:
-        if len(self.values) < number:
-            for item in self.values:
-                self.values = fill
+        result: list[float] = []
+        while len(result) < number:
+            result.append(fill)
+        self.values = result
 
-        return self.values 
+    def arange(self, start: float, stop: float, step: float = 1.0) -> None:
+        result: list[float] = []
+        assert step != 0.0
+        if step < 0:
+            while start > stop:
+                result.append(start)
+                start += step
+        elif step > 0:
+            while start < stop:
+                result.append(start)
+                start += step
+        self.values = result
+
+    def sum(self) -> float: 
+        result: float
+        result = sum(self.values)
+        return result
+
+    def __add__(self, rhs: Union[float, Simpy]) -> Simpy:
+        result: list[float] = []
+        if isinstance(rhs, float):
+            result: list[float] = []
+            for item in self.values:
+                result.append(item + rhs) 
+        else:
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                result.append(self.values[i] + rhs.values[i])
+        return Simpy(result)
+
+    def __pow__(self, rhs: Union[float, Simpy]) -> Simpy:
+        result: list[float] = []
+        if isinstance(rhs, float):
+            result: list[float] = []
+            for item in self.values:
+                result.append(item ** rhs) 
+        else:
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                result.append(self.values[i] ** rhs.values[i])
+        return Simpy(result)
+
+    def __mod__(self, rhs: Union[float, Simpy]) -> Simpy:
+        result: list[float] = []
+        if isinstance(rhs, float):
+            result: list[float] = []
+            for item in self.values:
+                result.append(item % rhs) 
+        else:
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                result.append(self.values[i] % rhs.values[i])
+        return Simpy(result)
+
+    def __eq__(self, rhs: Union[float, Simpy]) -> list[bool]:
+        result: list[bool] = []
+        if isinstance(rhs, float):
+            for item in self.values:
+                result.append(item == rhs) 
+        else:
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                result.append(self.values[i] == rhs.values[i])
+        return result
+
+    def __gt__(self, rhs: Union[float, Simpy]) -> list[bool]:
+        result: list[bool] = []
+        if isinstance(rhs, float):
+            for item in self.values:
+                result.append(item > rhs) 
+        else:
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                result.append(self.values[i] > rhs.values[i])
+        return result
+
+    def __getitem__(self, rhs: int) -> float:
+        result: float = 0.0
+        result = self[rhs]
+        return result
